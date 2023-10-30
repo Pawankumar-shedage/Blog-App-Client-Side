8dce553f04a23016effd5d6e2f67128407dac1f2
Pawankumar Shedage <pawankumarshedage@gmail.com>
2023-10-30 21:26:56 +0530

    Footer updated

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
   return (
     <>
-      <footer className="fixed-bottom bg-dark text-light py-4 pb-2">
+      <footer className="sticky-bottom bg-dark text-light py-4 pb-2">
         <div className="container text-center">
           <p>&copy; 2023 Get Hooked</p>
         </div>
